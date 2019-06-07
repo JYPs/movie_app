6ea00db502387a8e37e1a925b8da13eecf205445
JYPs <qkrwndma5@naver.com>
2019-06-07 16:13:52 +0900

    Updates

---
+++ precache-manifest.5f20c9fde9f87b75a032181f04714ceb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d53d9dcac0f0fd90d36d3c5f06989ec7",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "d392e762598ebf7f7195",
+    "url": "/movie_app/static/css/main.f069e77c.chunk.css"
+  },
+  {
+    "revision": "ad63ab08a2a8e5504836",
+    "url": "/movie_app/static/js/2.7b834e7d.chunk.js"
+  },
+  {
+    "revision": "d392e762598ebf7f7195",
+    "url": "/movie_app/static/js/main.a1630600.chunk.js"
+  },
+  {
+    "revision": "c6514544d436775abdc2",
+    "url": "/movie_app/static/js/runtime~main.175dda75.js"
+  }
+]);
